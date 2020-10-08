b974b7c1964152c13b4ab4ba0fa9fba379bb6446
Tatiane Lanzetti <tati.lanzetti@gmail.com>
2020-10-08 14:58:32 -0300

    Adicionado Splash Screen.

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,19 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Dashboard from './pages/Dashboard';
 import EditProduct from './pages/EditProduct';
 import NewProduct from './pages/NewProduct';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
